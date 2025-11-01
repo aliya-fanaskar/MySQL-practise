665a8e2c770ece23b0ff8ddf7135853127302160
aliya-fanaskar <aliyafan99@gmail.com>
2025-11-01 16:28:56 +0530

    modifications (01-11-25)

--- my-MySQL-practise/1_create_tables.sql
+++ my-MySQL-practise/1_create_tables.sql
@@ -4,7 +4,7 @@ USE company;                                -- select the database
 
 SHOW TABLES;            -- display the list of tables in that database
 
-
+-- --------------------------------------------------------------------------------------------------------
 # Employee details Table
 CREATE TABLE employee(
 	emp_id INT PRIMARY KEY NOT NULL,
@@ -16,30 +16,36 @@ CREATE TABLE employee(
     city CHAR(25)
 );
 
+-- inserting values into table (25 rows)
 INSERT INTO employee (emp_id, first_name, last_name, salary, joining_date, department, city) VALUES
-    (001, 'Monika', 'Arora', 100000, '2024-02-01 09:00:00', 'HR', 'Mumbai'),
-    (002, 'Niharika', 'Verma', 80000, '2024-06-10 09:00:00', 'Admin', 'Bangalore'),
-    (003, 'Vishal', 'Singhal', 300000, '2022-09-20 09:00:00', 'HR', 'Pune'),
-    (004, 'Amitabh', 'Singh', 500000, '2020-02-20 09:00:00', 'IT', 'Mumbai'),
-    (005, 'Vivek', 'Chaudhari', 90000, '2025-05-05 09:00:00', 'Admin', 'Hyderabad'),
-    (006, 'Vipul', 'Nair', 200000, '2023-06-13 09:00:00', 'Account', 'Pune'),
-    (007, 'Raj', 'Kumar', 75000, '2025-01-20 09:00:00', 'Account', 'Pune'),
-    (008, 'Geetika', 'Chauhan', 350000, '2023-04-05 09:00:00', 'IT', 'Mumbai'),
-    (009, 'Nikhil', 'Patel', 120000, '2023-06-20 09:00:00', 'Admin', 'Mumbai'),
-    (010, 'Sangita', 'Kumar', 95000, '2025-01-20 09:00:00', 'HR', 'Hyderabad'),
-    (011, 'Rekha', 'Kaur', 75000, '2025-05-20 09:00:00', 'Account', 'Bangalore'),
-    (012, 'Nikhil', 'Yadav', 100000, '2023-01-11 09:00:00', 'Admin', 'Mumbai'),
-    (013, 'Suman', 'Chaudhari', 95000, '2024-05-20 09:00:00', 'IT', 'Pune'),
-    (014, 'Manoj', 'Gupta', 80000, '2024-04-22 09:00:00', 'Admin', 'Hyderabad'),
-    (015, 'Rajesh', 'Sharma', 200000, '2022-01-11 09:00:00', 'IT', 'Mumbai'),
-    (016, 'Usha', 'Patel', 500000, '2019-04-11 09:00:00', 'IT', 'Bangalore'),
-    (017, 'Raj', 'Prasad', 450000, '2020-06-19 09:00:00', 'Admin', 'Pune'),
-    (018, 'Satish', 'Singh', 120000, '2022-06-08 09:00:00', 'Account', 'Pune'),
-    (019, 'Santosh', 'Kumar', 80000, '2024-09-11 09:00:00', 'IT', 'Bangalore'),
-    (020, 'Gita', 'Naik', 400000, '2020-09-22 09:00:00', 'HR', 'Mumbai');
+	(001, 'Monika', 'Arora', 100000, '2024-02-01 09:00:00', 'HR', 'Mumbai'),
+	(002, 'Neha', 'Khan', 80000, '2024-06-10 09:00:00', 'Admin', 'Bangalore'),
+	(003, 'Vishal', 'Singh', 300000, '2022-09-20 09:00:00', 'Operations', 'Pune'),
+	(004, 'Anand', 'Shinde', 500000, '2020-02-20 09:00:00', 'Accounts', 'Mumbai'),
+	(005, 'Vivek', 'Chaudhari', 90000, '2025-05-05 09:00:00', 'IT', 'Hyderabad'),
+	(006, 'Aman', 'Sharma', 120000, '2025-05-05 09:00:00', 'Operations', 'Hyderabad'),
+	(007, 'Raj', 'Kumar', 75000, '2025-01-20 09:00:00', 'Accounts', 'Pune'),
+	(008, 'Jessica', 'Dsouza', 350000, '2023-04-05 09:00:00', 'IT', 'Mumbai'),
+	(009, 'Niharika', 'Shetty', 120000, '2024-01-10 09:00:00', 'Sales', 'Bangalore'),
+	(010, 'Vidya', 'Pillai ', 550000, '2020-02-20 09:00:00', 'Operations', 'Chennai'),
+	(011, 'Aman', 'Khan', 75000, '2025-05-20 09:00:00', 'Accounts', 'Bangalore'),
+	(012, 'Monika', 'Dsouza', 200000, '2023-02-01 09:00:00', 'Sales', 'Chennai'),
+	(013, 'Suman', 'Chaudhari', 95000, '2024-05-20 09:00:00', 'IT', 'Pune'),
+	(014, 'John', 'Francis', 80000, '2024-04-22 09:00:00', 'Admin', 'Hyderabad'),
+	(015, 'Rajesh', 'Sharma', 200000, '2022-01-11 09:00:00', 'Operations', 'Mumbai'),
+	(016, 'Venkat', 'Iyer', 450000, '2022-09-20 09:00:00', 'Operations', 'Chennai'),
+	(017, 'Raj', 'Prasad', 420000, '2020-06-19 09:00:00', 'Sales', 'Pune'),
+	(018, 'Satish', 'Singh', 120000, '2022-06-08 09:00:00', 'Accounts', 'Pune'),
+	(019, 'Arjun', 'Shetty', 80000, '2024-09-11 09:00:00', 'IT', 'Bangalore'),
+	(020, 'Gita', 'Naik', 400000, '2020-09-22 09:00:00', 'Operations', 'Mumbai'),
+	(021, 'Nikhil', 'Yadav', 100000, '2023-01-11 09:00:00', 'Admin', 'Mumbai'),
+	(022, 'Jonathan', 'Fernandez', 120000, '2023-06-20 09:00:00', 'IT', 'Mumbai'),
+	(023, 'Usha', 'Pai', 600000, '2020-04-11 09:00:00', 'IT', 'Bangalore'),
+	(024, 'Sara', 'Hussain', 95000, '2025-01-20 09:00:00', 'HR', 'Hyderabad'),
+	(025, 'Vipul', 'Nair', 200000, '2023-06-13 09:00:00', 'IT', 'Pune');
 
-
- # Employee bonuses Table
+-- --------------------------------------------------------------------------------------------------------
+# Employee BONUS Table
 CREATE TABLE bonus(
 	emp_ref_id INT,
     bonus_amount INT,
@@ -59,8 +65,8 @@ INSERT INTO bonus (emp_ref_id, bonus_amount, bonus_date) VALUES
     (019, 4500, '2025-10-20'),
     (010, 3500, '2025-10-11');
 
-
-# Employee titles Table
+-- --------------------------------------------------------------------------------------------------------
+# Employee TITLE Table
 CREATE TABLE title(
 	emp_ref_id INT,
     emp_title CHAR(25),
@@ -83,7 +89,7 @@ INSERT INTO title (emp_ref_id, emp_title, affect_from) VALUES
     (006, 'Manager', '2025-02-01 00:00:00'),
     (003, 'Manager', '2025-09-15 00:00:00');
 
-
+-- --------------------------------------------------------------------------------------------------------
 -- view tables
 SHOW TABLES;
 
@@ -95,5 +101,7 @@ SELECT * FROM bonus ORDER BY emp_ref_id, bonus_amount;
 SELECT * FROM title;
 SELECT * FROM title ORDER BY emp_ref_id;
 
-DESC employee;            -- details of a certain table
+-- details about a certain table
+DESC employee;
 DESC title;
+
