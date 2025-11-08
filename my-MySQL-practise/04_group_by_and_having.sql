297eebf4d5ba3758f6836b236ccc9f4684584920
aliya-fanaskar <aliyafan99@gmail.com>
2025-11-08 19:00:19 +0530

    Update 04_group_by_and_having.sql

--- my-MySQL-practise/04_group_by_and_having.sql
+++ my-MySQL-practise/04_group_by_and_having.sql
@@ -2,33 +2,36 @@
 ---------------------------------------------------------------------------------------------------------------------------
 Filename         : 04_group_by_and_having.sql
 Level            : Intermediate
-Concepts covered : GROUP BY clause, GROUP_CONCAT function, HAVING clause, handling ties using Subqueries with GROUP BY and HAVING clause
+Concepts covered : GROUP BY clause, GROUP_CONCAT function, HAVING clause
 ---------------------------------------------------------------------------------------------------------------------------
 */
 
 USE company;          -- select the database
+SELECT * FROM employee LIMIT 5;
 
-SELECT * FROM employee;
-
--- Display the no. of employees from Mumbai city
-SELECT COUNT(*) AS Mumbai_based_emps FROM employee WHERE city = 'Mumbai';
-
--- Display No. of employees in the 'Admin' department.
-SELECT count(*) AS admin_dept_count FROM employee WHERE department = 'Admin';
 
 -- ---------------------------------------------------------------------------------------------------------------------------------------------
 # GROUP BY clause
+-- ---------------
 /*The GROUP BY clause in SQL is used to group rows that have the 
 same values in one or more specified columns into summary rows. 
 This is frequently used in conjunction with aggregate functions 
 to perform calculations on these grouped data. */
 
--- Display the no. of employees from each city
+-- For Example: To get the count of employees from Mumbai city, we write the following query
+SELECT COUNT(*) AS Mumbai_based_emps FROM employee WHERE city = 'Mumbai';
+
+-- similarly, To get the count of employees in the Admin department, we write the following query
+SELECT count(*) AS admin_dept_count FROM employee WHERE department = 'Admin';
+
+-- But what if we need the total count of employees from each city altogether? This is where the GROUP BY is used 
 SELECT
 	city,
     count(*)
 FROM employee
 GROUP BY city;
+
+-- Keep in mind that all non-aggregated columns in the SELECT list must appear in the GROUP BY clause
 
 
 -- Display the no. of employees in each department
@@ -55,6 +58,7 @@ FROM employee
 GROUP BY department
 ORDER BY No_of_employees;
 
+
 -- Count of employees in different departments from Mumbai city.
 SELECT 
 	department,
@@ -63,6 +67,7 @@ FROM employee
 WHERE city = 'Mumbai'
 GROUP BY department
 ORDER BY from_Mumbai;
+
 
 -- get each department's count of employees, total salaries and highest and lowest salary
 SELECT 
@@ -75,6 +80,7 @@ FROM employee
 GROUP BY department
 ORDER BY nos;
 
+
 -- From which city does the company have the highest number of employees?
 SELECT 
 	city,
@@ -83,6 +89,7 @@ FROM employee
 GROUP BY city
 ORDER BY nos DESC
 LIMIT 1;
+
 
 -- From which city does the company have the lowest number of employees?
 SELECT 
@@ -103,6 +110,7 @@ FROM employee
 GROUP BY department, city
 ORDER BY nos, department;
 
+
 -- ---------------------------------------------------------------------------------------------------------------------------------------------
 # GROUP_CONCAT
 /* 
@@ -116,6 +124,12 @@ SYNTAX :- GROUP_CONCAT([DISTINCT] expression [ORDER BY expression ASC|DESC] [SEP
     SEPARATOR → Defines the string separator (default is a comma ,)
 */
 -- get the names of employees from each list
+SELECT 
+	city,
+    GROUP_CONCAT(first_name) 
+FROM employee 
+GROUP BY city;  -- group by discussed in another file
+
 SELECT 
 	city,
     GROUP_CONCAT(first_name ORDER BY first_name SEPARATOR ', ') 
@@ -134,27 +148,30 @@ ORDER BY frequency;  -- group by discussed in another file
 
 
 -- ---------------------------------------------------------------------------------------------------------------------------------------------
-# HAVING
+# HAVING Clause
+-- -------------
 /* The HAVING clause in SQL is used to filter groups of rows 
 based on a specified condition, typically involving aggregate functions. 
-It is used in conjunction with the GROUP BY clause.
-Unlike the WHERE clause, which filters individual rows before grouping, 
-HAVING filters the results after the GROUP BY clause has aggregated the data into groups.*/
+It is used in conjunction with the GROUP BY clause.*/
 
--- Get the count of employees in each department.
+-- For Example: To get the count of employees in each department, we write the following query
 SELECT
 	department,
     count(*)
 FROM employee
 GROUP BY department;
 
- -- Fetch the department(s) with only 2 employees.
+ -- However to get the count of employees from department(s) with only 2 employees.
 SELECT 
 	department,
     COUNT(*) AS frequency
 FROM employee
 GROUP BY department
 HAVING frequency = 2;
+
+/* Unlike the WHERE clause, which filters individual rows before grouping, 
+HAVING filters the results after the GROUP BY clause has aggregated the data into groups.
+Keep in mind that WHERE filters rows before grouping, while HAVING filters groups after aggregation*/
 
 -- Display the number of employees from each city and filter only those cities having 5 or more employees.
 SELECT
@@ -164,6 +181,7 @@ FROM employee
 GROUP BY city
 HAVING employees >= 5;
 
+
 -- Display the number of employees from each city and filter only those cities having employees between the range of 3 to 6.
 SELECT
 	city, 
@@ -171,6 +189,7 @@ SELECT
 FROM employee 
 GROUP BY city
 HAVING employees BETWEEN 3 AND 6;
+
 
 -- Department-wise total salaries
 SELECT
@@ -189,6 +208,7 @@ GROUP BY department
 HAVING dept_cost > 1000000
 ORDER BY dept_cost;
 
+
 -- Write a query to display the number of employees working in each department and city.
 -- Show only those department–city combinations where the count of employees is greater than 1. 
 SELECT
@@ -200,6 +220,7 @@ GROUP BY department, city
 HAVING nos > 1
 ORDER BY department;
 
+
 -- Query to find repeated last names and show how many employees share each one
 SELECT 
 	last_name,
@@ -207,49 +228,3 @@ SELECT
 FROM employee
 GROUP BY last_name
 HAVING frequency > 1;
-
-
--- ---------------------------------------------------------------------------------------------------------------------------------------------
-# Handling ties with Subqueries...
--- Which department has the highest no. of employees?
-SELECT department, COUNT(*) AS nos
-FROM employee
-GROUP BY department
-ORDER BY nos DESC
-LIMIT 1;
-
--- but if you check the count...
-SELECT department, COUNT(*) FROM employee GROUP BY department ORDER BY COUNT(*) DESC;
--- there are ties... 
-
--- to get the department(s) with the highest no. of employees (by handling ties):
-
--- STEP 1: get the department-wise count. You will get a column list of count values
--- basic syntax: SELECT COUNT(*) FROM table GROUP BY col;
-SELECT COUNT(*) AS dept_nos
-FROM employee
-GROUP BY department;
-
--- STEP 2: get the highest count from the column list of counts using max()
--- basic syntax: SELECT MAX(col) FROM table;
- SELECT MAX(dept_nos) 
- FROM (
-	SELECT COUNT(*) AS dept_nos 
-    FROM employee 
-    GROUP BY department
-    ) AS dept_count_table;  -- the column acts as 1D table so don't forget to ALIAS the derived-table
-
--- STEP 3: filter groups of rows from the entire employee table HAVING the max value.
--- basic syntax: SELECT cols FROM table GROUP BY col HAVING condtion;
-SELECT department, count(*) AS nos 
-FROM employee 
-GROUP BY department
-HAVING nos = (
-	SELECT MAX(dept_nos)
-    FROM (
-		SELECT COUNT(*) AS dept_nos 
-        FROM employee 
-        GROUP BY department) AS dept_count_table
-);
-
--- ... the concept of subquery is discussed in detail in further files
